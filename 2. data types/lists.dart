83bcd573f5b00c457f4b141dc4f3c4c6ddc2293c
권남희 <knaem.edu@gmail.com>
2025-03-13 13:15:52 +0900

    Lists

---
+++ 2. data types/lists.dart
@@ -0,0 +1,14 @@
+void main() {
+  // List<int> numbers = [1, 2, 3, 4];
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    // collection if
+    if (giveMeFive) 5, // giveMeFive가 true이면 5 추가
+  ];
+  print(numbers);
+  // numbers.add(1);
+}
